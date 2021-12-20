744dcc294fd341a8a4dd4e597f10afc1442bea97
manjulahumane <manjulab.xworkz@gmail.com>
2021-12-20 17:39:50 +0530

    upload age file

---
+++ Age.java
@@ -0,0 +1,16 @@
+class Age{
+static byte[]person={11,12,33,44,55,66,77,88,99,48};
+public static void main(String[] args){
+System.out.println(person[0]);
+System.out.println(person[1]);
+System.out.println(person[3]);
+System.out.println(person[4]);
+System.out.println(person[5]);
+System.out.println(person[6]);
+System.out.println(person[7]);
+System.out.println(person[8]);
+System.out.println(person[9]);
+
+}
+
+}
